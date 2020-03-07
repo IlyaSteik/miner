fe903a04d8992a7dd2bca34691d6a9d81f9a7f7a
Admin <ilya.steik4@gmail.com>
2020-03-07 22:46:14 +0300

    Updates

---
+++ precache-manifest.bf1450d8ceca18e21d5d21ff41d9883a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4809950634792c765382924416c074",
+    "url": "/miner/index.html"
+  },
+  {
+    "revision": "af91869c53ec65ccfebd",
+    "url": "/miner/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "af91869c53ec65ccfebd",
+    "url": "/miner/static/js/2.e6971741.chunk.js"
+  },
+  {
+    "revision": "970f839d00b893dbbfb2",
+    "url": "/miner/static/js/main.455a96ff.chunk.js"
+  },
+  {
+    "revision": "9414588e6249c3025e89",
+    "url": "/miner/static/js/runtime-main.fa61a78d.js"
+  }
+]);
